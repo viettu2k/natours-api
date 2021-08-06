00b4f4817d2ecdfeb6213db8f3ae9b819435e2f2
viettu2k <doanviettu2k@gmail.com>
2021-08-06 10:09:32 +0700

    use static file with exporess.static

--- app.js
+++ app.js
@@ -8,8 +8,8 @@ const app = express();
 
 // 1) MIDDLEWARE
 app.use(morgan('dev'));
-
 app.use(express.json());
+app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
     console.log('hello from middleware');
