f7a615e734cb83c4c99b9c9b50dde1e173839674
viettu2k <doanviettu2k@gmail.com>
2021-08-14 15:47:32 +0700

    creating tour from tourController

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -12,7 +12,7 @@ const router = express.Router();
 
 // router.param('id', checkID);
 
-router.route('/').get(getAllTours).post(checkBody, createTour);
+router.route('/').get(getAllTours).post(createTour);
 
 router.route('/:id').get(getTour).patch(updateTour).delete(deleteTour);
 
