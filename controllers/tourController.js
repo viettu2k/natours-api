f13b8722da72d5d8c46ebd200f643139049d420a
viettu2k <doanviettu2k@gmail.com>
2021-08-18 09:45:58 +0700

    making the api better filtering

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -2,7 +2,23 @@ const Tour = require('./../models/tourModel');
 
 exports.getAllTours = async (req, res) => {
   try {
-    const tours = await Tour.find();
+    // BUILD QUERY
+    const queryObj = { ...req.query };
+    const excludedFiedlds = ['page', 'sort', 'limit', 'fields'];
+    excludedFiedlds.forEach((el) => delete queryObj[el]);
+
+    const query = await Tour.find(queryObj);
+
+    // const query = await Tour.find()
+    //     .where('duration')
+    //     .equals(5)
+    //     .where('difficulty')
+    //     .equals('easy');
+
+    // EXECUTE QUERY
+    const tours = await query;
+
+    // SEND RESPONSE
     res.status(200).json({
       status: 'success',
       requestedAt: req.requestTime,
--- dev-data/data/import-dev-data.js
+++ dev-data/data/import-dev-data.js
@@ -6,50 +6,50 @@ const Tour = require('./../../models/tourModel');
 dotenv.config({ path: './config.env' });
 
 const DB = process.env.DATABASE.replace(
-    '<PASSWORD>',
-    process.env.DATABASE_PASSWORD
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
 );
 
 mongoose
-    .connect(DB, {
-        useNewUrlParser: true,
-        useCreateIndex: true,
-        useFindAndModify: false,
-        useUnifiedTopology: true,
-    })
-    .then(() => console.log('DB connection successfull'));
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log('DB connection successfull'));
 
 // READ JSON FILE
 const tours = JSON.parse(
-    fs.readFileSync(`${__dirname}/tours-simple.json`, 'utf-8')
+  fs.readFileSync(`${__dirname}/tours-simple.json`, 'utf-8')
 );
 
 // IMPORT DATA TO DB
-const importData = async() => {
-    try {
-        await Tour.create(tours);
-        console.log('Data successfully loaded!');
-    } catch (e) {
-        console.log(e);
-    }
-    process.exit();
+const importData = async () => {
+  try {
+    await Tour.create(tours);
+    console.log('Data successfully loaded!');
+  } catch (e) {
+    console.log(e);
+  }
+  process.exit();
 };
 
 // DELETE ALL DATA FROM DB
-const deleteData = async() => {
-    try {
-        await Tour.deleteMany();
-        console.log('Data successfully deleted!');
-    } catch (e) {
-        console.log(e);
-    }
-    process.exit();
+const deleteData = async () => {
+  try {
+    await Tour.deleteMany();
+    console.log('Data successfully deleted!');
+  } catch (e) {
+    console.log(e);
+  }
+  process.exit();
 };
 
 if (process.argv[2] === '--import') {
-    importData();
+  importData();
 } else if (process.argv[2] === '--delete') {
-    deleteData();
+  deleteData();
 }
 
 console.log(process.argv);
